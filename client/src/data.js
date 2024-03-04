b9bdc6e260bf9c02c65fac8e3908d4d1ae68598e
Akash Singh <developer.akash8@gmail.com>
2024-03-04 22:27:18 +0530

    iterate through all the clubs

---
+++ client/src/data.js
@@ -0,0 +1,152 @@
+export const clubs = {
+  ai: {
+    title: "AI Club",
+    type: "technical",
+    description:
+      "The AI Club at VIT Bhopal University is a community of forward-thinkers and innovators passionate about Artificial Intelligence (AI). Our mission is to tackle industrial challenges and pave the way for a better future through AI-driven solutions. Join us to explore the limitless potential of AI in various domains, from healthcare and finance to automation and robotics. Through workshops, projects, and industry collaborations, we aim to equip members with the skills and knowledge needed to drive technological advancements and make a positive impact on society. Be a part of the AI revolution and shape the future together!",
+    coordinator: "Dr. A V R Mayuri (100255)",
+    president: "Sandeep Balabantaray",
+    members: 129,
+    hiring: false,
+  },
+  android: {
+    title: "Android",
+    type: "technical",
+    description:
+      "The Android Club at VIT Bhopal University is for aspiring app developers and tech enthusiasts who gather here to dive into the exciting world of Android. Our club offers a hands-on learning experience, where members can create innovative mobile applications, explore the latest trends in Android development, and collaborate on real-world projects. Through workshops, coding challenges, and guest lectures from industry experts, we aim to nurture talent and empower students to excel in the ever-evolving Android ecosystem. Join us to unlock your potential, share ideas, and be part of shaping the future of mobile technology!",
+    coordinator: "Dr. G.Ganeshan (100377)",
+    president: "Harsh Raj",
+    members: 48,
+    hiring: true,
+  },
+  iot: {
+    title: "IoT Club",
+    type: "technical",
+    description:
+      "At IoT Club our mission is to empower students with the transformative potential of the Internet of Things (IoT). Through hands-on projects, workshops, and mentorship, we delve into the world of connected devices and smart systems. Join us to explore the integration of sensors, actuators, and data analytics to create innovative IoT solutions. As a community of tech enthusiasts, we aim to cultivate a deeper understanding of IoT’s real-world applications and its impact on various industries. Together, let’s embrace the IoT revolution and shape a smarter and connected future!",
+    coordinator: "Dr. Ankur Jain ( 100397 )",
+    president: "Akshat Sinha",
+    members: 129,
+    hiring: true,
+  },
+  ios: {
+    title: "IoS Club",
+    type: "technical",
+    description:
+      "The AI Club at VIT Bhopal University is a community of forward-thinkers and innovators passionate about Artificial Intelligence (AI). Our mission is to tackle industrial challenges and pave the way for a better future through AI-driven solutions. Join us to explore the limitless potential of AI in various domains, from healthcare and finance to automation and robotics. Through workshops, projects, and industry collaborations, we aim to equip members with the skills and knowledge needed to drive technological advancements and make a positive impact on society. Be a part of the AI revolution and shape the future together!",
+    coordinator: "Dr E Nirmala (100326)",
+    president: "Sandeep Balabantaray",
+    members: 129,
+    hiring: false,
+  },
+  microsoft: {
+    title: "Microsoft Technical Club",
+    type: "technical",
+    description:
+      "As a hub for technology enthusiasts, we explore the vast world of Microsoft technologies and products. Join us to delve into the latest developments in software, cloud computing, AI, and more. Our club hosts workshops, coding competitions, and hackathons, empowering members to master Microsoft tools and platforms. Whether you’re interested in app development, data analytics, or enterprise solutions, we provide the resources and support to fuel your technical journey. Be part of our innovative community, learn from industry experts, and elevate your technical expertise with the Microsoft Technical Club!",
+    coordinator: "Dr. Hariharasitaraman S (100237)",
+    president: "Anay Verma ",
+    members: 129,
+    hiring: false,
+  },
+  fusion: {
+    title: "Fusion Club",
+    type: "non-technical",
+    description:
+      "Fusion Club at VIT Bhopal is an event management club focused on creating innovative experiences for students. We organize diverse events, blending cultural festivals with sports activities, catering to various interests. With a dedicated team and a strong creative drive, Fusion Club is committed to delivering fresh and engaging activities to the VIT Bhopal campus, ensuring students have a memorable and enjoyable time.",
+    coordinator: "Dr. Karthik Rao M C (100425)",
+    president: "Sudhith N ",
+    members: 129,
+    hiring: false,
+  },
+  insights: {
+    title: "Insights Club",
+    type: "non-technical",
+    description:
+      "Insights Club is the journalism and media club at VIT Bhopal University, providing students with a platform to contribute to the community while gaining valuable experience, skills, and networks in journalism. Our primary objective is to host media-related events and competitions, delivering information on university events, current affairs, and hot topics through online and offline news channels. We publish monthly newsletters covering seminars, fests, and ongoing events. Our secondary objective is to partner with other clubs for successful events and publish reports in the yearly magazine. At Insights, we embrace creativity, nurture ideas, and encourage individuals with a passion for journalism and media to join our dynamic environment.",
+    coordinator: "Dr. I. Jasmine Selvakumari Jeya (100278) ",
+    president: "Atharva Awatade ",
+    members: 129,
+    hiring: false,
+  },
+  photography: {
+    title: "Photography & Movie Making Club",
+    type: "non-technical",
+    description:
+      "The Photography & Movie Making Club at VIT Bhopal University is a group of visual storytellers and enthusiasts, celebrating the art of photography and filmmaking. Join us to explore the world of visual expression, from capturing stunning images to creating compelling movies. Our club offers workshops, film screenings, and hands-on projects to sharpen your skills and creativity. Whether you’re a seasoned photographer or a budding filmmaker, all are welcome to indulge in the magic of visual storytelling. Let’s capture moments and bring stories to life together at the Photography & Movie Making Club!",
+    coordinator: "Mr. Mayank Gupta(100016)",
+    president: " ",
+    members: 129,
+    hiring: false,
+  },
+  dance: {
+    title: "Dance Club",
+    type: "non-technical",
+    description:
+      "Get ready to witness the power of movements as we come together to celebrate the art of dance. Whether you’re a seasoned dancer or someone who loves to groove, our club welcomes all to embrace the rhythm and expression of dance. Join us to explore various dance styles, from contemporary and hip-hop to traditional and cultural dances. Through dance workshops, performances, and dance-offs, we aim to unleash the energy and creativity that dance brings to our lives. Let’s unite through the language of dance and create memorable moments on the dance floor at the Dance Club!",
+    coordinator: "Dr. Yogesh Shukla(100130)",
+    president: "Saachi Duggal",
+    members: 129,
+    hiring: false,
+  },
+  cooking: {
+    title: "Cooking and Feasting Club",
+    type: "non-technical",
+    description:
+      "The Cooking and Feasting Club is a group of food enthusiasts who share a love for cooking and indulging in delicious feasts. Join us to explore diverse cuisines, culinary techniques, and culinary cultures from around the world. Through cooking workshops, food tasting events, and recipe exchanges, we aim to enhance your culinary skills and expand your gastronomic horizons. Whether you’re a beginner or an experienced home chef, our club provides a welcoming space to connect with like-minded foodies and embrace the joy of cooking and feasting together. Let’s embark on a flavorful journey of culinary delights at the Cooking and Feasting Club!",
+    coordinator: "Dr. Monica Sankat (100537) ",
+    president: "Dev Charan Sai P ",
+    members: 129,
+    hiring: false,
+  },
+  music: {
+    title: "Music & Band Club",
+    type: "non-technical",
+    description:
+      "The music club offers both vocal and instrumental musicians a stage to showcase their talents, encouraging the development of their musical abilities. The club supports its members’ diverse talents in several fields and boasts a very wide roster of musicians, unconstrained by any one musical genre, including guitarists, keyboardists, bassists, drummers and vocalists. The music club also manages the audio for a variety of events of other clubs and even supplies the necessary assistance for their execution.",
+    coordinator: "Dr. Suchetana Sadhukhan (100284) ",
+    president: "Subhangini guin ",
+    members: 129,
+    hiring: false,
+  },
+  hindi: {
+    title: "Hindi Club",
+    type: "regional",
+    description:
+      "The Hindi Club at VIT Bhopal University is a vibrant community of Hindi enthusiasts dedicated to celebrating the richness and beauty of the Hindi language and culture. Our club provides a platform for students to come together, express themselves, and promote Hindi language and literature. Through various cultural events, poetry recitals, debates, and workshops, we strive to nurture a deeper appreciation for Hindi among the college community. Whether you are a native Hindi speaker or have a passion for the language, all are welcome to join us in preserving and promoting the spirit of Hindi at the Hindi Club. Let’s come together and embrace the beauty of Hindi literature and culture!",
+    coordinator: "Dr. Rajeev Saxena (100081) ",
+    president: "Yuvraj Singh Rathore ",
+    members: 129,
+    hiring: false,
+  },
+  rajasthan: {
+    title: "Mharo Rajasthan Club",
+    type: "regional",
+    description:
+      "The Maharo Rajasthan Club at our college is a student-led organization dedicated to promoting and celebrating the culture and traditions of Rajasthan. We provide a platform for students interested in Rajasthani culture to come together, share knowledge and skills, and organize various events and activities.",
+    coordinator: "Dr. Hemlata Gangwal (100325) ",
+    president: "Yuvraj Singh Rathore ",
+    members: 129,
+    hiring: false,
+  },
+  gujarat: {
+    title: "Gujarati Club",
+    type: "regional",
+    description:
+      "The Gujarati Club of VIT Bhopal is a vibrant and dynamic student club dedicated to celebrating and promoting Gujarati culture. Through a variety of cultural events, we showcase the richness and diversity of Gujarat’s traditions, art, music, and dance. ",
+    coordinator: "Dr Bhumika Girishkumar Choksi (100190) ",
+    president: "Vairag Patel ",
+    members: 129,
+    hiring: false,
+  },
+  marathi: {
+    title: "Marathi Club",
+    type: "regional",
+    description:
+      "The Marathi Club at VIT Bhopal University is a passionate community dedicated to celebrating the rich culture and heritage of Maharashtra and the Marathi language. Our club provides a platform for students to come together, connect, and share their love for Marathi literature, art, music, and traditions. Through various cultural events, workshops, and discussions, we strive to promote Marathi culture and language on campus.",
+    coordinator: "Dr. Narendrakumar (100462)",
+    president: "Pankaj Patil",
+    members: 129,
+    hiring: false,
+  },
+};
